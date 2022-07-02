85258f56fbc732cb2c52a4622a867bb85dd8ed76
Isaac A. Adewuyi <99651336+IsaacAdewuyi@users.noreply.github.com>
2022-07-02 04:14:35 +0100

    HTML and JS

---
+++ main.js
@@ -0,0 +1,176 @@
+
+// Heading
+
+let title = "Workers"
+document.getElementById("title").innerHTML = title;
+
+
+// Table Heading
+function Table_Head(){
+    const heading = [
+        "No", "First Name", "Last Name", "Age", "Sex", "Position", "Date of Employment"
+    ]
+    
+    document.getElementById("No").innerHTML = heading[0]
+    document.getElementById("F_Name").innerHTML = heading[1]
+    document.getElementById("L_Name").innerHTML = heading[2]
+    document.getElementById("Age").innerHTML = heading[3]
+    document.getElementById("Sex").innerHTML = heading[4]
+    document.getElementById("Position").innerHTML = heading[5]
+    document.getElementById("Date_Of_Emp").innerHTML = heading[6]
+}
+Table_Head()
+
+// Table Body
+
+function Row_one(){
+    const row1 = [
+        "1.", "Thompson", "Anthony", 32, "Male", "Head of Finance Department", "17/07/2014"
+    ]
+    document.getElementById("no1").innerHTML = row1[0]
+    document.getElementById("f_name1").innerHTML = row1[1]
+    document.getElementById("l_name1").innerHTML = row1[2]
+    document.getElementById("age1").innerHTML = row1[3]
+    document.getElementById("sex1").innerHTML = row1[4]
+    document.getElementById("post1").innerHTML = row1[5]
+    document.getElementById("doe1").innerHTML = row1[6]
+}
+Row_one()
+
+function Row_two(){
+    const row2 = [
+        "2.", "Johnson", "Mary", 23, "Female", "Software Engineer", "05/03/2019"
+    ]
+    document.getElementById("no2").innerHTML = row2[0]
+    document.getElementById("f_name2").innerHTML = row2[1]
+    document.getElementById("l_name2").innerHTML = row2[2]
+    document.getElementById("age2").innerHTML = row2[3]
+    document.getElementById("sex2").innerHTML = row2[4]
+    document.getElementById("post2").innerHTML = row2[5]
+    document.getElementById("doe2").innerHTML = row2[6]
+}
+Row_two()
+
+
+function Row_three(){
+    const row3 = [
+        "3.", "Chisom", "Onyeka", 28, "Female", "Head of Human Resource", "21/06/2016"
+    ]
+    document.getElementById("no3").innerHTML = row3[0]
+    document.getElementById("f_name3").innerHTML = row3[1]
+    document.getElementById("l_name3").innerHTML = row3[2]
+    document.getElementById("age3").innerHTML = row3[3]
+    document.getElementById("sex3").innerHTML = row3[4]
+    document.getElementById("post3").innerHTML = row3[5]
+    document.getElementById("doe3").innerHTML = row3[6]
+}
+Row_three()
+
+
+function Row_four(){
+    const row4 = [
+        "4.", "Jide", "Olabisi", 29, "Male", "Assistant Head(Software Department)", "05/06/2016"
+    ]
+    document.getElementById("no4").innerHTML = row4[0]
+    document.getElementById("f_name4").innerHTML = row4[1]
+    document.getElementById("l_name4").innerHTML = row4[2]
+    document.getElementById("age4").innerHTML = row4[3]
+    document.getElementById("sex4").innerHTML = row4[4]
+    document.getElementById("post4").innerHTML = row4[5]
+    document.getElementById("doe4").innerHTML = row4[6]
+}
+Row_four()
+
+
+function Row_five(){
+    const row5 = [
+        "5.", "Williams", "Ajayi", 30, "Male", "Head of Software Department", "06/09/2015"
+    ]
+    document.getElementById("no5").innerHTML = row5[0]
+    document.getElementById("f_name5").innerHTML = row5[1]
+    document.getElementById("l_name5").innerHTML = row5[2]
+    document.getElementById("age5").innerHTML = row5[3]
+    document.getElementById("sex5").innerHTML = row5[4]
+    document.getElementById("post5").innerHTML = row5[5]
+    document.getElementById("doe5").innerHTML = row5[6]
+}
+Row_five()
+
+
+function Row_six(){
+    const row6 = [
+        "6.", "Nancy", "chukwuemeka", 27, "Female", "Secretary", "23/08/2016"
+    ]
+    document.getElementById("no6").innerHTML = row6[0]
+    document.getElementById("f_name6").innerHTML = row6[1]
+    document.getElementById("l_name6").innerHTML = row6[2]
+    document.getElementById("age6").innerHTML = row6[3]
+    document.getElementById("sex6").innerHTML = row6[4]
+    document.getElementById("post6").innerHTML = row6[5]
+    document.getElementById("doe6").innerHTML = row6[6]
+}
+Row_six()
+
+
+function Row_seven(){
+    const row7 = [
+        "7.", "Nicholas", "Uchemba", 26, "Male", "Software Engineer", "22/02/2020"
+    ]
+    document.getElementById("no7").innerHTML = row7[0]
+    document.getElementById("f_name7").innerHTML = row7[1]
+    document.getElementById("l_name7").innerHTML = row7[2]
+    document.getElementById("age7").innerHTML = row7[3]
+    document.getElementById("sex7").innerHTML = row7[4]
+    document.getElementById("post7").innerHTML = row7[5]
+    document.getElementById("doe7").innerHTML = row7[6]
+}
+Row_seven()
+
+
+function Row_eight(){
+    const row8 = [
+        "8.", "Ahmed", "Olayinka", 24, "Male", "Graphic Designer", "15/08/2020"
+    ]
+    document.getElementById("no8").innerHTML = row8[0]
+    document.getElementById("f_name8").innerHTML = row8[1]
+    document.getElementById("l_name8").innerHTML = row8[2]
+    document.getElementById("age8").innerHTML = row8[3]
+    document.getElementById("sex8").innerHTML = row8[4]
+    document.getElementById("post8").innerHTML = row8[5]
+    document.getElementById("doe8").innerHTML = row8[6]
+}
+Row_eight()
+
+
+
+function Row_nine(){
+    const row9 = [
+        "9.", "Halima", "Muhammed", 26, "Female", "Senior Finance Manager", "30/05/2018"
+    ]
+    document.getElementById("no9").innerHTML = row9[0]
+    document.getElementById("f_name9").innerHTML = row9[1]
+    document.getElementById("l_name9").innerHTML = row9[2]
+    document.getElementById("age9").innerHTML = row9[3]
+    document.getElementById("sex9").innerHTML = row9[4]
+    document.getElementById("post9").innerHTML = row9[5]
+    document.getElementById("doe9").innerHTML = row9[6]
+}
+Row_nine()
+
+
+function Row_ten(){
+    const row10 = [
+        "10.", "Isaac", "Adewuyi", 22, "Male", "C.E.O", "17/11/2010"
+    ]
+    document.getElementById("no10").innerHTML = row10[0]
+    document.getElementById("f_name10").innerHTML = row10[1]
+    document.getElementById("l_name10").innerHTML = row10[2]
+    document.getElementById("age10").innerHTML = row10[3]
+    document.getElementById("sex10").innerHTML = row10[4]
+    document.getElementById("post10").innerHTML = row10[5]
+    document.getElementById("doe10").innerHTML = row10[6]
+}
+Row_ten()
+
+
+
